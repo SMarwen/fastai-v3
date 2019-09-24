45e59b0b6de3e7733dd7e41aff266e8d40535da4
SMarwen <sallem.marwen@yahoo.fr>
2019-09-24 13:33:14 +0200

    fix google drive link

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1iyTO0zwUTE7tmwR4golmmyEmgBsME5Ho/view?usp=sharing'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1iyTO0zwUTE7tmwR4golmmyEmgBsME5Ho'
 export_file_name = 'export.pkl'
 
 classes = ['barilla', 'panzani']
