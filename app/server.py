60397247e539f93773e6452182e1cb7814899290
SMarwen <sallem.marwen@yahoo.fr>
2019-09-24 16:34:59 +0200

    fix torch version and google rive url

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1p3llTMKbY4ZizcdaemmvS2tVlqgfCti8'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Yox3olBLiZCLTEjBEjw-Vw-zLblMvTqB'
 export_file_name = 'export.pkl'
 
 classes = ['barilla', 'panzani']
